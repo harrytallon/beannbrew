92ec6a9774975ba6b9400d78004f75e84d06bf03
harrytallon <s250767@greenhead.ac.uk>
2025-09-24 14:25:20 +0100

    Added a custom rounded button property

---
+++ WpfApp1/RoundedButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public class RoundedButton : Button
+    {
+        public static readonly DependencyProperty CornerProperty =
+            DependencyProperty.Register(nameof(Corner), typeof(CornerRadius), typeof(RoundedButton), new PropertyMetadata(new CornerRadius(0)));
+
+        public CornerRadius Corner
+        {
+            get => (CornerRadius)GetValue(CornerProperty);
+            set => SetValue(CornerProperty, value);
+        }
+
+        static RoundedButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RoundedButton), new FrameworkPropertyMetadata(typeof(RoundedButton)));
+        }
+    }
+}
